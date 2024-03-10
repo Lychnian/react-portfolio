b446bb5f10d52a45327751b0c20d183f80ededf4
Helen Colon <helenvcolon@gmail.com>
2024-03-10 16:57:04 -0400

    Edited About jsx css Portfolio jsx css added media queries

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,97 +5,72 @@ import Portfolio3 from "../../img/employeetracker.png";
 import Portfolio4 from "../../img/ecommerce.png";
 import Portfolio5 from "../../img/timedquiz.png";
 import Portfolio6 from "../../img/socialnetwork.png";
-
-import "../../css/portfolio.css";
-
-const styles = {
-  h2: {
-    textAlign: "center",
-    color: "white",
-  },
-  space: {
-    paddingLeft: "0.5rem",
-    paddingRight: "0.5rem",
-  },
-  header: {
-    textAlign: "center",
-    paddingBottom: "0.15rem",
-    color: "black",
-  },
- };
+import "../../css/portfolio.css"; // Import the CSS file for styles
 
 export default function Portfolio() {
   return (
-    <div style={{ backgroundColor: "black" }}>
-      <h2 style={styles.h2}>Portfolio</h2>
-      <span className="worksDesc" style={{ color: "white" }}>
-        I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. 
-        I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.
+    <div className="portfolio-background">
+      <h2 className="portfolio-title">Portfolio</h2>
+      <span className="portfolio-description">I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.
       </span>
       <div className="container">
         <div className="row justify-content-center">
+          {/* Project 1 */}
           <div className="col-sm-6">
-            <section className="card col-sm-3 p-0 m-2 d-flex flex-column justify-content-center align-items-center position-relative">
+            <section className="portfolio-card">
               <a href="https://garden-planner19-05823e4485e2.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio1} alt="image of a beautiful garden" className="card-img-top img-fluid" />
+                <img src={Portfolio1} alt="Garden Planner" className="card-img-top" />
+                <header>Garden Planner</header>
               </a>
-              <header className="position-absolute bottom-0 w-100 text-center">
-                <h3 style={styles.header}>Garden Planner</h3>
-              </header>
             </section>
           </div>
+          {/* Project 2 */}
           <div className="col-sm-6">
-            <section className="card col-sm-3 p-0 m-2 d-flex flex-column justify-content-center align-items-center position-relative">
+            <section className="portfolio-card">
               <a href="https://lychnian.github.io/wise-weather-dashboard/" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio2} alt="" className="card-img-top img-fluid" />
-              </a> 
-              <header className="position-absolute bottom-0 w-100 text-center">
-                <h3 style={styles.header}>Weather Dashboard</h3>
-              </header>
+                <img src={Portfolio2} alt="Weather Dashboard" className="card-img-top" />
+                <header>Weather Dashboard</header>
+              </a>
             </section>
           </div>
         </div>
         <div className="row justify-content-center">
+          {/* Project 3 */}
           <div className="col-sm-6">
-            <section className="card col-sm-3 p-0 m-2 d-flex flex-column justify-content-center align-items-center position-relative">
+            <section className="portfolio-card">
               <a href="https://github.com/Lychnian/janeaustentechco-employee-tracker" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio3} alt="A man’s open hand holding a network of interconnected nodes, with human figurines, symbolizing the interconnectedness of employees in the company." className="card-img-top img-fluid" />
+                <img src={Portfolio3} alt="Employee Tracker" className="card-img-top" />
+                <header>Employee Tracker</header>
               </a>
-              <header className="position-absolute bottom-0 w-100 text-center">
-                <h3 style={styles.header}>Employee Tracker</h3>
-              </header>
             </section>
           </div>
+          {/* Project 4 */}
           <div className="col-sm-6">
-            <section className="card col-sm-3 p-0 m-2 d-flex flex-column justify-content-center align-items-center position-relative">
+            <section className="portfolio-card">
               <a href="https://github.com/Lychnian/server-side-commerce" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio4} alt="A laptop with a little model of a shopping cart on it." className="card-img-top img-fluid" />
-              </a> 
-              <header className="position-absolute bottom-0 w-100 text-center">
-               <h3 style={styles.header}>e-Commerce</h3>
-              </header>
+                <img src={Portfolio4} alt="e-Commerce Site" className="card-img-top" />
+                <header>e-Commerce</header>
+              </a>
             </section>
           </div>
-        </div> 
+        </div>
         <div className="row justify-content-center">
+          {/* Project 5 */}
           <div className="col-sm-6">
-            <section className="card col-sm-3 p-0 m-2 d-flex flex-column justify-content-center align-items-center position-relative">
+            <section className="portfolio-card">
               <a href="https://lychnian.github.io/js-timed-quiz/" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio5} alt="" className="card-img-top img-fluid" />
+                <img src={Portfolio5} alt="Timed Quiz" className="card-img-top" />
+                <header>Timed Quiz</header>
               </a>
-              <header className="position-absolute bottom-0 w-100 text-center">
-                <h3 style={styles.header}>Timed Quiz</h3>
-              </header>
             </section>
           </div>
+          {/* Project 6 */}
           <div className="col-sm-6">
-            <section className="card col-sm-3 p-0 m-2 d-flex flex-column justify-content-center align-items-center position-relative">
+            <section className="portfolio-card">
               <a href="https://github.com/Lychnian/travel-social-network" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio6} alt="A group of employees around a conference table holding out to each other large cards of colorful icons. There are laptops and mobile devices on the table." className="card-img-top img-fluid" />
+                <img src={Portfolio6} alt="Social Network for Travelers" className="card-img-top" />
+                <header>Social Network</header>
               </a>
-              <header className="position-absolute bottom-0 w-100 text-center">
-                <h3 style={styles.header}>Social Network</h3>
-              </header>
             </section>
           </div>
         </div>
