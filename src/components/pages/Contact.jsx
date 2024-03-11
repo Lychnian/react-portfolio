32714419194c31a2f263997688efaa452d9edc05
Helen Colon <helenvcolon@gmail.com>
2024-03-10 22:49:37 -0400

    Edited about, footer, header, portfolio css and contact jsx

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,45 +1,6 @@
 import React, { useState } from "react";
 import { validateEmail } from "../../utils/helpers";
-
-const styles = {
-  container: {
-    backgroundColor: "black",
-    color: "white",
-    textAlign: "center",
-  },
-  h1: {
-    textAlign: "center",
-    color: "yellow",
-    fontSize: "40px",
-  },
-  form: {
-    textAlign: "center",
-  },
-  email: {
-    textAlign: "center",
-    color: "#0000FF",
-  },
-  input: {
-    margin: "0.5rem",
-    backgroundColor: "white",
-    color: "black",
-    padding: "0.5rem",
-    borderRadius: "5px",
-  },
-  inputa: {
-    height: "40px",
-  },
-  paragraph: {
-    fontSize: "18px",
-    fontStyle: "italic",
-    color: "#666",
-    margin: "0.5rem",
-  },
-  successText: {
-    color: "green",
-    margin: "0.5rem",
-  },
-};
+import '../../css/contact.css';
 
 function Form() {
   const [email, setEmail] = useState("");
@@ -90,55 +51,57 @@ function Form() {
   };
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.h1}>Contact Me</h1>
-      <p style={styles.paragraph}>Please fill out the form below to contact me for work opportunities.</p>
-      <div style={styles.email}>
-        <a href="mailto:HelenColon@test.123">HelenColon@test.123</a>
+    <div className="container">
+      <div className="contact-container">
+        <h1 className="contact-title">Contact Me</h1>
+        <p className="contact-paragraph">Please fill out the form below to contact me for work opportunities.</p>
+        <div className="contact-email">
+          <a href="mailto:HelenColon@test.123">HelenColon@test.123</a>
+        </div>
+        <form action="mailto:HelenColon@test.123" method="post" encType="text/plain" className="contact-form">
+          <div className="input">
+            <input
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+              placeholder="email"
+              required
+            />
+          </div>
+          <div className="input">
+            <input
+              value={Name}
+              name="Name"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="name"
+              required
+            />
+          </div>
+          <div className="input">
+            <input
+              className="inputa"
+              value={message}
+              name="message"
+              onChange={handleInputChange}
+              type="message"
+              placeholder="message"
+              required
+            />
+          </div>
+          <div>
+            <button type="button" onClick={handleFormSubmit}>
+              Submit
+            </button>
+         </div>
+        </form>
+        {successMessage && (
+          <div className="success-text">
+            <p>{successMessage}</p>
+          </div>
+        )}
       </div>
-      <form action="mailto:HelenColon@test.123" method="post" encType="text/plain" style={styles.form} className="form">
-        <div style={styles.input}>
-          <input
-            value={email}
-            name="email"
-            onChange={handleInputChange}
-            type="email"
-            placeholder="email"
-            required
-          />
-        </div>
-        <div style={styles.input}>
-          <input
-            value={Name}
-            name="Name"
-            onChange={handleInputChange}
-            type="text"
-            placeholder="name"
-            required
-          />
-        </div>
-        <div style={styles.input}>
-          <input
-            style={styles.inputa}
-            value={message}
-            name="message"
-            onChange={handleInputChange}
-            type="message"
-            placeholder="message"
-            required
-          />
-        </div>
-        <div>
-          <button type="button" onClick={handleFormSubmit}>
-            Submit
-          </button>
-        </div>
-      </form>
-      {successMessage && (
-        <div style={styles.successText}>
-          <p>{successMessage}</p>
-        </div>
-      )}
     </div>
   );
 }
